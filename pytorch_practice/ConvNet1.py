bbce4f52654afe64b49ba7aa63821438fa9a3621
llzxo <llzxp0614@gmail.com>
2017-05-09 17:06:53 +0800

    update

--- pytorch_practice/ConvNet1.py
+++ pytorch_practice/ConvNet1.py
@@ -1,7 +1,23 @@
 import torch
 from torch.autograd import Variable
 import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
 import torch.nn.functional as F
+
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('./mnist', train=True, download=True, transform=transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307, ), (0.3081, ))
+    ])), batch_size=64, shuffle=True
+)
+
+test_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('./mnist', train=False, transform=transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307, ), (0.3081, ))
+    ])), batch_size=1000, shuffle=True,
+)
 
 class MNISTConvNet(nn.Module):
     def __init__(self):
@@ -10,29 +26,72 @@ class MNISTConvNet(nn.Module):
         self.pool1 = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(10, 20, 5)
         self.pool2 = nn.MaxPool2d(2, 2)
+        self.conv2_drop = nn.Dropout2d()
         self.fc1 = nn.Linear(320, 50)
         self.fc2 = nn.Linear(50, 10)
 
-    def forward(self, input):
-        x = self.pool1(F.relu(self.conv1(input)))
-        x = self.pool2(F.relu(self.conv2(x)))
+    def forward(self, x):
+        x = F.relu(self.pool1(self.conv1(x)))
+        x = F.relu(self.pool2(self.conv2_drop(self.conv2(x))))
         x = x.view(x.size(0), -1)
         x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
         x = F.relu(self.fc2(x))
-        return x
+        return F.log_softmax(x)
 
 net = MNISTConvNet()
 print(net)
+if torch.cuda.is_available():
+    net = net.cuda()
 
-inputs = Variable(torch.randn(1, 1, 28, 28))
-out = net(inputs)
-print(out.size())
+optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
 
-target = Variable(torch.LongTensor([3]))
-loss_fn = nn.CrossEntropyLoss()
-err = loss_fn(out, target)
-err.backward()
-print(err)
+def train(epoch):
+    net.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        if torch.cuda.is_available():
+            data, target = data.cuda(), target.cuda()
+        data, target = Variable(data), Variable(target)
+        optimizer.zero_grad()
+        output = net(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 50 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                       100. * batch_idx / len(train_loader), loss.data[0]))
 
-print(net.conv1.weight.data.norm())
-print(net.conv1.weight.grad.data.norm())
+def test(epoch):
+    net.eval()
+    test_loss = 0
+    correct = 0
+    for data, target in test_loader:
+        if torch.cuda.is_available():
+            data, target = data.cuda(), target.cuda()
+        data, target = Variable(data, volatile=True), Variable(target)
+        output = net(data)
+        test_loss += F.nll_loss(output, target).data[0]
+        pred = output.data.max(1)[1]
+        correct += pred.eq(target.data).cpu().sum()
+    test_loss = test_loss
+    test_loss /= len(test_loader)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+for epoch in range(1,100):
+    train(epoch)
+    test(epoch)
+# inputs = Variable(torch.randn(1, 1, 28, 28))
+# out = net(inputs)
+# print(out.size())
+#
+# target = Variable(torch.LongTensor([3]))
+# loss_fn = nn.CrossEntropyLoss()
+# err = loss_fn(out, target)
+# err.backward()
+# print(err)
+#
+# print(net.conv1.weight.data.norm())
+# print(net.conv1.weight.grad.data.norm())
--- pytorch_practice/Transfer.py
+++ pytorch_practice/Transfer.py
@@ -7,11 +7,9 @@ from torch.autograd import Variable
 import numpy as np
 import torchvision
 from torchvision import datasets, models, transforms
-import matplotlib.pyplot as plt
 import time
 import copy
 import os
-
 
 data_transforms = {
     'train': transforms.Compose([
@@ -31,10 +29,92 @@ data_transforms = {
 data_dir = 'hymenoptera_data'
 dset = {x: datasets.ImageFolder(os.path.join(data_dir,x), data_transforms[x])
          for x in ['train', 'val']}
-dset_loaders = {x: torch.utils.data.DataLoader(dset[x], batch_size = 4, shuffle=True, num_workers=4)
+dset_loaders = {x: torch.utils.data.DataLoader(dset[x], batch_size = 4, shuffle=True, num_workers=4)#shuffle: data sequence
                 for x in ['train', 'val']}
 dset_sizes = {x: len(dset[x]) for x in ['train', 'val']}
 dset_classes = dset['train'].classes
 
 use_gpu = torch.cuda.is_available()
 
+def train_model(model, criterion, optimizer, lr_scheduler, num_epochs=25):
+    since = time.time()
+
+    best_model = model
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                optimizer = lr_scheduler(optimizer, epoch)
+                model.train(True)
+            else:
+                model.train(False)
+
+            running_loss = 0.0
+            running_corrects = 0
+            for data in dset_loaders[phase]:
+                inputs, labels = data
+                if use_gpu:
+                    inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
+                else:
+                    inputs, labels = Variable(inputs), Variable(labels)
+
+                optimizer.zero_grad()
+
+                outputs = model(inputs)
+                _, pre = torch.max(outputs.data, 1)
+                loss = criterion(outputs, labels)
+
+                if phase == 'train':
+                    loss.backward()
+                    optimizer.step()
+
+                running_loss += loss.data[0]
+                running_corrects += torch.sum(pre == labels.data)
+
+            epoch_loss = running_loss / dset_sizes[phase]
+            epoch_acc = running_corrects / dset_sizes[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model = copy.deepcopy(model)
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+    return best_model
+
+def exp_lr_scheduler(optimizer, epoch, init_lr=0.001, lr_decay_epoch=7):
+    lr = init_lr * (0.1**(epoch // lr_decay_epoch))
+
+    if epoch % lr_decay_epoch == 0:
+        print('LR is set to {}'.format(lr))
+
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+    return optimizer
+
+# train_model(model=models.resnet18(pretrained=True).cuda(), criterion=nn.CrossEntropyLoss(),
+#             optimizer=optim.SGD(models.resnet18().parameters(), lr=0.001, momentum=0.9),
+#             lr_scheduler=exp_lr_scheduler, num_epochs=25)
+
+model_ft = models.resnet18(pretrained=True)
+num_ftrs = model_ft.fc.in_features
+model_ft.fc = nn.Linear(num_ftrs, 2)
+
+if use_gpu:
+    model_ft = model_ft.cuda()
+
+criterion = nn.CrossEntropyLoss()
+
+# Observe that all parameters are being optimized
+optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+
+model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
+                       num_epochs=25)
